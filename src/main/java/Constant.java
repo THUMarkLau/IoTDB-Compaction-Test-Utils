2a920de6e5bc89b03de854c416845e90474a84c2
Liu Xuxin <liuxuxin@outlook.com>
2021-10-27 16:55:58 +0800

    init commit

---
+++ src/main/java/Constant.java
@@ -0,0 +1,38 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+public class Constant {
+  public final static String SG_NAME = "root.sg_test";
+  public final static String DEVICE_PATTERN = "d%d";
+  public final static String MEASUREMENT_PATTERN = "s%d";
+  public final static String INSERT_SQL_PATTERN = "insert into %s(timestamp, %s), values (%d, %d)";
+  public final static String FLUSH_SQL = "flush";
+
+  public static String getDeviceName(int deviceId) {
+    return String.format(SG_NAME.concat(".") + DEVICE_PATTERN, deviceId);
+  }
+
+  public static String getMeasurementName(int deviceId, int measurementId) {
+    return String.format(SG_NAME.concat(".") + DEVICE_PATTERN.concat(".") + MEASUREMENT_PATTERN, deviceId, measurementId);
+  }
+
+  public static String getInsertSql(int deviceId, int measurementId, int timestamp, int value) {
+    return String.format(INSERT_SQL_PATTERN, getDeviceName(deviceId), String.format(MEASUREMENT_PATTERN, measurementId), timestamp, value);
+  }
+}
---
+++ src/main/java/DataGenerationUtils.java
@@ -0,0 +1,97 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import org.apache.iotdb.rpc.IoTDBConnectionException;
+import org.apache.iotdb.rpc.StatementExecutionException;
+import org.apache.iotdb.session.Session;
+import org.apache.iotdb.tsfile.file.metadata.enums.TSDataType;
+import org.apache.iotdb.tsfile.write.record.Tablet;
+import org.apache.iotdb.tsfile.write.schema.MeasurementSchema;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class DataGenerationUtils {
+
+  public static void generateUnseqData(Session session, int seqFileNum, int unseqFileNum, int deviceNum, int measurementNum, long pointInFileForEachTS) throws IoTDBConnectionException, StatementExecutionException {
+    long startTimeForSeqData = pointInFileForEachTS * unseqFileNum;
+    long endTimeForSeqData = startTimeForSeqData + pointInFileForEachTS * seqFileNum;
+    for(long time = startTimeForSeqData; time < endTimeForSeqData;) {
+      for(int i = 0; i < deviceNum; ++i) {
+        List<MeasurementSchema> schemaList = new ArrayList<>();
+        for(int j = 0; j < measurementNum; ++j) {
+          schemaList.add(new MeasurementSchema(String.format(Constant.MEASUREMENT_PATTERN, j), TSDataType.INT64));
+        }
+        Tablet tablet = new Tablet(Constant.getDeviceName(i), schemaList, 100);
+        for (long row = time; row < time + 100; row++) {
+          int rowIndex = tablet.rowSize++;
+          tablet.addTimestamp(rowIndex, row);
+          for (int j = 0; j < measurementNum; ++j) {
+            long value = new Random().nextLong();
+            tablet.addValue(schemaList.get(j).getMeasurementId(), rowIndex, value);
+          }
+          if (tablet.rowSize == tablet.getMaxRowNumber()) {
+            session.insertTablet(tablet, true);
+            tablet.reset();
+          }
+        }
+        if (tablet.rowSize != 0) {
+          session.insertTablet(tablet);
+          tablet.reset();
+        }
+      }
+      time += 100;
+      if (time != startTimeForSeqData && time % pointInFileForEachTS == 0) {
+        session.executeNonQueryStatement("flush");
+      }
+    }
+    session.executeNonQueryStatement("flush");
+    for(long time = 0; time < startTimeForSeqData;) {
+      for(int i = 0; i < deviceNum; ++i) {
+        List<MeasurementSchema> schemaList = new ArrayList<>();
+        for(int j = 0; j < measurementNum; ++j) {
+          schemaList.add(new MeasurementSchema(String.format(Constant.MEASUREMENT_PATTERN, j), TSDataType.INT64));
+        }
+        Tablet tablet = new Tablet(Constant.getDeviceName(i), schemaList, 100);
+        for (long row = time; row < time + 100; row++) {
+          int rowIndex = tablet.rowSize++;
+          tablet.addTimestamp(rowIndex, row);
+          for (int j = 0; j < measurementNum; ++j) {
+            long value = new Random().nextLong();
+            tablet.addValue(schemaList.get(j).getMeasurementId(), rowIndex, value);
+          }
+          if (tablet.rowSize == tablet.getMaxRowNumber()) {
+            session.insertTablet(tablet, true);
+            tablet.reset();
+          }
+        }
+        if (tablet.rowSize != 0) {
+          session.insertTablet(tablet);
+          tablet.reset();
+        }
+      }
+      time += 100;
+      if (time % pointInFileForEachTS == 0) {
+        session.executeNonQueryStatement("flush");
+      }
+    }
+    session.executeNonQueryStatement("flush");
+  }
+}
